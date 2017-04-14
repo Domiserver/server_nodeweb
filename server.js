b7c164422d198697375780d03d8619f184074ac4
norvaweb <norvatel@gmail.com>
2017-04-14 06:08:34 -0400

    Setup start script and server port number

--- server.js
+++ server.js
@@ -2,26 +2,14 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', hbs);
 app.use(express.static(__dirname + '/public'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-Ihbs.registerHelper('getCurrentYear', () => {
+hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
 });
 
@@ -42,6 +30,6 @@ app.get('/about', (req, res) => {
       });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
     });
